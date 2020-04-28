e7e489d0460d3b17422c4553edcf5999165731bc
ElReyZero1201 <31524106+ElReyZero1201@users.noreply.github.com>
2020-04-28 14:28:34 -0500

    Add files via upload

--- visionex/credentials.py
+++ visionex/credentials.py
@@ -1,11 +1,6 @@
-from urllib.request import urlopen
-page = r"C:\Users\Juan PC\Desktop\Lood\visionex\apikey.json" #Ruta de la llave de atuenticación
-def explicit():
-    from google.cloud import storage
-    # Explicitly use service account credentials by specifying the private key
-    # file.
-    storage_client = storage.Client.from_service_account_json(page)
-
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
+from google.cloud import vision
+from google.oauth2 import service_account
+creds = service_account.Credentials.from_service_account_file('./key.json') #Aquí va la ruta a la llave de autenticación de la api
+client = vision.ImageAnnotatorClient(
+   credentials=creds,
+)
