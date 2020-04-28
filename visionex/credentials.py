7a46decebdb596ea70b6f2d4aa1d39daff610610
ElReyZero1201 <31524106+ElReyZero1201@users.noreply.github.com>
2020-04-28 13:36:05 -0500

    Update credentials.py

--- visionex/credentials.py
+++ visionex/credentials.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen
-content = urlopen("https://raw.githubusercontent.com/ElReyZero1201/pyfunct/master/apikey.json")
-page = r"C:\Users\Juan PC\Desktop\Lood\visionex\apikey.json"
+page = r"C:\Users\Juan PC\Desktop\Lood\visionex\apikey.json" #Ruta de la llave de atuenticación
 def explicit():
     from google.cloud import storage
     # Explicitly use service account credentials by specifying the private key
