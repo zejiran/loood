6d693002f99568b5b7d38d9d1cbf95d84f25a790
zejiran <juanszalegria@gmail.com>
2020-05-07 14:38:40 -0500

    version 1

--- preinstall.js
+++
@@ -1,3 +0,0 @@
-// reads auth info for Google Cloud Vision API to a file so the @google-cloud/vision sdk could use it
-const fs=require('fs'); 
-fs.writeFile('./google-credentials-vision.json', process.env.GOOGLE_CONFIG, (err) => {});
